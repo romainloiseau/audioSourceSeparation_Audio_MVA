3072e9c2b02d70ee0bcab80d695b19ecc0795655
tangih <tangi.hetet@polytechnique.edu>
2019-03-06 17:50:23 +0100

    EM rewriting

--- code/em.py
+++ code/em.py
@@ -2,93 +2,112 @@ import numpy as np
 from tqdm import tqdm_notebook
 
 
-def compute_Sc(W, H):
-    F, K = W.shape
-    _, N = H.shape
+def W_H_masked(W, H, j, Kpart):
+    ind = np.cumsum(Kpart)
+    prev = 0
+    if j > 0:
+        prev = ind[j-1]
+    return W[:, prev:ind[j]], H[prev:ind[j]]
+
+
+def compute_sigma_s(W, H, F, N, J, Kpart):
+    sigma_s = np.zeros((F, N, J, J), dtype=np.float)
+    for j in range(J):
+        W_masked, H_masked = W_H_masked(W, H, j, Kpart)
+        sigma_s[:, :, j, j] = W_masked.dot(H_masked)
+    return sigma_s
+
+
+def compute_sigma_c(W, H, F, N, K):
+    sigma_c = np.zeros((F, N, K, K))
+    for k in range(K):
+        sigma_c[:, :, k, k] = W[:, k].reshape((F, 1)).dot(H[k].reshape((1, N)))
+    return sigma_c                                                   
     
-    W = np.array([W for n in range(N)]).transpose(1, 0, 2)
-    H = np.array([H for f in range(F)]).transpose(0, 2, 1)
-    return W * H
-
-
-def compute_Arond(A, K, Kcal):
-    I, _, F = A.shape
-    Arond = np.zeros((I, K, F), dtype=complex)
-    for j, k in enumerate(Kcal):
-        Arond[:, k] = A[:, j]
+    
+def compute_Arond(A, K, Kpart):
+    F, I, J = A.shape
+    ind = np.cumsum(Kpart)
+    prev = 0
+    Arond = np.zeros((F, I, K), dtype=complex)
+    for j, k in enumerate(Kpart):
+        cur = ind[j]
+        Arond[:, :, prev:cur] = np.repeat(A[:, :, j].reshape(F, I, 1), cur-prev-1, axis=-1)        
+        prev = cur
     return Arond
 
 
-def compute_E_step(x_four, A, W, H, Sb, Kcal):
-    Sc = compute_Sc(W, H)
+def compute_sigma_x_fn(a_f, sigma_s_fn, sigma_b_f):
+    return a_f.dot(sigma_s_fn.dot(a_f.getH())) + sigma_b_f
+
+
+def compute_gs_fn(sigma_s_fn, sigma_x_fn, a_f):
+    # TODO: computation trick in the overdetermined case
+    sig_x_inv = np.linalg.inv(sigma_x_fn)
+    return sigma_s_fn.dot(a_f.getH().dot(sig_x_inv))
     
-    F, N, K = Sc.shape
-    I, J, _ = A.shape
     
-    Ss = Sc[:, :, Kcal]
-    Arond = compute_Arond(A, K, Kcal)
-    # TODO: how are we supposed to initialize the parameters ?
-    # (EM method only guarantees convergence to a critical point)
-    Gs = np.empty((F, N, J, I), dtype=complex)
-    Gc = np.empty((F, N, K, I), dtype=complex)
-    s = np.empty((F, N, J), dtype=complex)
-    c = np.empty((F, N, K), dtype=complex)
+def compute_gc_fn(sigma_c_fn, sigma_x_fn, arond_f):
+    sig_x_inv = np.linalg.inv(sigma_x_fn)
+    return sigma_c_fn.dot(arond_f.getH().dot(sig_x_inv))
+
+            
+def r_hat(x1, x2 = None):
+    if x2 == None:
+        x2 = x1
     
-    Rxs = np.empty((F, I, J), dtype=complex)
-    Rss = np.empty((F, J, J), dtype=complex)
-    u = np.empty((K, F, N), dtype=complex)
+    return np.sum([x1[i].dot(x2[i].getH())
+                                for i in range(x1.shape[0])]) / x1.shape[1]
+
+
+def compute_E_step(X, A, W, H, sigma_b, Kpart):
+    F, I, J = A.shape
+    K, N = H.shape
     
+    sigma_c = compute_sigma_c(W, H, F, N, K)
+    sigma_s = compute_sigma_s(W, H, F, N, J, Kpart)
+    Arond = compute_Arond(A, K, Kpart)
+    
+    Rxx = np.zeros((F, I, I), dtype=np.float)
+    Rxs = np.zeros((F, I, J), dtype=np.float)
+    Rss = np.zeros((F, J, J), dtype=np.float)
+    U = np.zeros((F, N, K), dtype=np.float)
+    S = np.zeros((F, N, J), dtype=np.float)
+
     for f in tqdm_notebook(range(F)):
+        c_f = np.zeros((N, K), dtype=np.float)
         for n in range(N):
-            a = np.matrix(A[:, :, f])
-            arond = np.matrix(Arond[:, :, f])
-            ss = np.diag(Ss[f, n])
-            sc = np.diag(Sc[f, n])
+            sigma_x_fn = compute_sigma_x_fn(A[f], sigma_s[f, n], sigma_b[f])
+            gs_fn = compute_gs_fn(sigma_s[f, n], sigma_x_fn, A[f])
+            gc_fn = compute_gc_fn(sigma_c[f, n], sigma_x_fn, Arond[f])
             
-            sx = a.dot(ss).dot(a.getH()) + Sb[f]
-            invsx = np.linalg.inv(sx)
+            S[f, n] = gs_fn.dot(X[f, n])
+            c_f[n] = gc_fn.dot(X[f, n])
+            U[f, n] = np.diagonal(c_f[n].dot(c_f[n].getH()) + sigma_c[f, n] - gc_fn.dot(Arond[f].dot(sigma_c[f, n])))
             
-            # TODO: woodbury matrix trick in the overdetermined case
-            Gs[f, n] = ss.dot(a.getH()).dot(invsx)
-            
-            Gc[f, n] = sc.dot(arond.getH()).dot(invsx)
-            
-            s[f, n] = Gs[f, n].dot(x_four[:, f, n])
-            c[f, n] = Gc[f, n].dot(x_four[:, f, n])
-            
-            Rxs[f] += np.matrix(x_four[:, f, n]).transpose().dot(np.matrix(s[f, n]).getH().transpose()) / N
-            
-            Rss[f] += np.matrix(s[f, n]).transpose().dot(np.matrix(s[f, n]).getH().transpose()) / N
-            Rss[f] += ss
-            Rss[f] -= Gs[f, n].dot(a).dot(ss)
-            
-            u[:, f, n] = np.matrix(c[f, n]) * np.matrix(c[f, n]).getH() + \
-                         np.diagonal(sc + Gc[f, n].dot(arond).dot(sc))
-            
-    return Rxs, Rss, u
+        Rxx[f] = r_hat(X[f])
+        Rxs[f] = r_hat(X[f], S[f])
+        Rss[f] = r_hat(s_f) + sigma_s[f, n] - gs_fn.dot(A[f].dot(sigma_s[f, n]))
+        
+    return Rxx, Rxs, Rss, U, S
 
     
-def compute_M_step(Rxx, Rxs, Rss, u, H, W):
+def compute_M_step(Rxx, Rxs, Rss, U, W, H):
     F, I, J = Rxs.shape
     K, N = H.shape
     
-    A = np.empty((I, J, F), dtype=complex)
-    Sb = np.empty((F, I, I), dtype=complex)
-
+    A = np.zeros((F, I, J), dtype=np.float)
+    sigma_b = np.zeros((F, I, I), dtype=np.float)
+    W = np.zeros((F, K), dtype=np.float)
+    K = np.zeros((K, N), dtype=np.float)
+    
     for f in range(F):
+        A[f] = Rxs[f].dot(np.linalg.inv(Rss[f]))
+        sigma_b[f] = np.diagonal(Rxx[f] - A[f].dot(Rxs[f].getH()) - Rxs[f].dot(A[f].getH()) + A[f].dot(Rss[f].dot(A[f].getH())))
+        for k in range(K):
+            W[f, k] = (1/N) * np.sum(np.divide(U[k, f], H[k]))
+    for k in range(K):
+        for n in range(N):
+            H[k, n] = (1/F) * np.sum(np.divide(U[k, :, n], W[:, n]))
 
-        A[:, :, f] = Rxs[f].dot(np.linalg.inv(Rss[f]))
-
-        af = np.matrix(A[:, :, f])
-        rxsf = np.matrix(Rxs[f])
-        
-        Sb[f] = - np.array(af.dot(rxsf.getH())) - np.array(rxsf.dot(af.getH())) \
-                + np.array(af.dot(Rss[f]).dot(af.getH()))
-        Sb[f] += np.diag(Rxx[f, f])
-        
-        W[f] = np.sum(u[:, f] / H, axis = -1) / N
-        
-    for n in range(N):
-        H[:, n] = np.sum(u[:, :, n] / W.transpose(), axis = -1) / F
-            
-    return A, Sb, H, W
+    return A, sigma_b, W, H
--- code/utils.py
+++ code/utils.py
@@ -2,19 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wave
 
-def plot_stft(Z):
-    
+def plot_stft(freqs, Z):
+    # TODO: change y axis in the plot in order to display the true frequencies (they are in the list freqs returned by the STFT)
     plt.figure(figsize = (15, 5))
     eps = 1e-10
     for i in range(Z.shape[0]):
         plt.subplot(101 + Z.shape[0] * 10 + i)
-
         logmag = np.flipud(np.log(eps + np.real(Z[i])**2))
         plt.imshow(logmag, extent=[0, logmag.shape[-1], 0, 512], aspect = 'auto', cmap = plt.cm.gist_heat)
         plt.title("STFT Spectrogram for channel {}".format(i))
         plt.ylabel("Frequencies")
         plt.xlabel("Time")
     plt.show()
+    
+    
+    
+#################################################################################
+#################################################################################
+#################################################################################
+#################################################################################
+    
     
     
 """
